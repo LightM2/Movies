d495d5bae5fd44761d8da34fffd5a2e9758ca4c6
LightM2 <sweta20002000@gmail.com>
2019-05-11 16:29:52 +0300

    filtered

--- app/src/main/java/com/svitlanamozharovska/movies/Main2Activity.kt
+++ app/src/main/java/com/svitlanamozharovska/movies/Main2Activity.kt
@@ -12,6 +12,9 @@ import java.util.ArrayList
 class Main2Activity : AppCompatActivity() {
 
     var dataList = ArrayList<DataModel>()
+    var yearFilters = listOf<String>()
+    var directorsFilters = listOf<String>()
+    var genresFilters = listOf<String>()
 
     lateinit var recyclerView: RecyclerView
     lateinit var recyclerViewGenres: RecyclerView
@@ -25,47 +28,41 @@ class Main2Activity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
 
-        recyclerView= findViewById(R.id.recyclerView)
-        recyclerView.adapter= DataAdapter(dataList,this)
-        recyclerView.layoutManager= LinearLayoutManager(this)
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.adapter = DataAdapter(dataList, this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        recyclerViewYears = findViewById(R.id.recyclerviewYears)
+        recyclerViewYears.layoutManager = LinearLayoutManager(this)
+
+        recyclerViewDirectors = findViewById(R.id.recyclerviewDirectors)
+        recyclerViewDirectors.layoutManager = LinearLayoutManager(this)
+
+        recyclerViewGenres = findViewById(R.id.recyclerviewGenres)
+        recyclerViewGenres.layoutManager = LinearLayoutManager(this)
 
 
         val filters = intent.getStringExtra(FiltersActivity.TOTAL_FILTERS)
-        if (filters!=null){
+        if (filters != null) {
             val listOfFilters = stringToList(filters)
-            callWebService(listOfFilters)
             val filtersName = FiltersName()
-            val yearFilters = sortFitlers(listOfFilters,filtersName.years)
-            if (yearFilters.isNotEmpty()){
-                recyclerViewYears = findViewById(R.id.recyclerviewYears)
-                recyclerViewYears.adapter = AdapterForMain2(yearFilters, this)
-                recyclerViewYears.layoutManager = LinearLayoutManager(this)
-            }
-            val directorsFilters = sortFitlers(listOfFilters,filtersName.directors)
-            if (directorsFilters.isNotEmpty()){
-                recyclerViewDirectors = findViewById(R.id.recyclerviewDirectors)
-                recyclerViewDirectors.adapter = AdapterForMain2(directorsFilters, this)
-                recyclerViewDirectors.layoutManager = LinearLayoutManager(this)
-            }
-            val genresFilters = sortFitlers(listOfFilters,filtersName.genres)
-            if (genresFilters.isNotEmpty()){
-                recyclerViewGenres = findViewById(R.id.recyclerviewGenres)
-                recyclerViewGenres.adapter= AdapterForMain2(genresFilters,this)
-                recyclerViewGenres.layoutManager= LinearLayoutManager(this)
-            }
-
-
-
-
+            yearFilters = sortFitlers(listOfFilters, filtersName.years)
+            recyclerViewYears.adapter = AdapterForMain2(yearFilters, this)
+            directorsFilters = sortFitlers(listOfFilters, filtersName.directors)
+            recyclerViewDirectors.adapter = AdapterForMain2(directorsFilters, this)
+            genresFilters = sortFitlers(listOfFilters, filtersName.genres)
+            recyclerViewGenres.adapter = AdapterForMain2(genresFilters, this)
+            callWebService()
 
 
         }
     }
-    fun sortFitlers(filters: List<String>, mainFilters:List<String>): MutableList<String> {
+
+    fun sortFitlers(filters: List<String>, mainFilters: List<String>): MutableList<String> {
         var sortedFilters = mutableListOf<String>()
-        for(i in 0 until filters.size){
-            for (j in 0 until mainFilters.size){
-                if (filters[i].equals(mainFilters[j])){
+        for (i in 0 until filters.size) {
+            for (j in 0 until mainFilters.size) {
+                if (filters[i].equals(mainFilters[j])) {
                     sortedFilters.add(filters[i])
                 }
             }
@@ -73,7 +70,8 @@ class Main2Activity : AppCompatActivity() {
         return sortedFilters
 
     }
-    fun callWebService(filters:List<String>) {
+
+    fun callWebService() {
         val apiService = ApiInterface.create()
         val call = apiService.getMovies()
         call.enqueue(object : Callback<CategoryResponse> {
@@ -81,23 +79,59 @@ class Main2Activity : AppCompatActivity() {
                 if (response != null) {
                     val newList = ArrayList<DataModel>()
                     newList.addAll(response.body()!!.values!!)
-                    for (i in 0 until filters.size){
-                        for (j in 0 until newList.size){
-                            val year = newList[j].year.toString()
-                            if (filters[i].equals(year)){
-                                dataList.add(newList[j])
+
+                    if (yearFilters.isNotEmpty()) {
+                        for (i in 0 until yearFilters.size) {
+                            for (j in 0 until newList.size) {
+                                val year = newList[j].year.toString()
+                                if (yearFilters[i].equals(year)&& !dataList.contains(newList[j])) {
+                                    dataList.add(newList[j])
+                                }else if (year.equals("All")){
+                                    dataList.clear()
+                                    dataList.addAll(newList)
+                                }
                             }
                         }
                     }
-                    dataList.add(newList[1])
-                    dataList.add(newList[2])
+
+                    if (directorsFilters.isNotEmpty()) {
+                        for (i in 0 until directorsFilters.size) {
+                            for (j in 0 until newList.size) {
+                                val director = newList[j].director
+                                if (directorsFilters[i].equals(director)&& !dataList.contains(newList[j])) {
+                                    dataList.add(newList[j])
+                                }else if (director.equals("All")){
+                                    dataList.clear()
+                                    dataList.addAll(newList)
+                                }
+                            }
+                        }
+
+                    }
+
+                    if (genresFilters.isNotEmpty()) {
+                        for (i in 0 until genresFilters.size) {
+                            for (j in 0 until newList.size) {
+                                val genres = newList[j].genre
+                                for (k in 0 until genres.size){
+                                    val genr = genres[k]
+                                    if (genresFilters[i].equals(genr)&& !dataList.contains(newList[j])){
+                                        dataList.add(newList[j])
+                                    }else if (genr.equals("All")){
+                                        dataList.clear()
+                                        dataList.addAll(newList)
+                                    }
+                                }
+                            }
+                        }
+                    }
                     recyclerView.adapter?.notifyDataSetChanged()
                 }
 
             }
 
             override fun onFailure(call: Call<CategoryResponse>, t: Throwable) {
-                Log.d("onFailure","onFailure")
+                Log.d("onFailure", "onFailure")
             }
         })
     }
